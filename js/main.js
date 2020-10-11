7485f4e6f735e20916b079bb52df939dc357e718
Vlad Dedikov <69351396+crew-dev@users.noreply.github.com>
2020-10-11 10:12:43 +0300

    upd//0.1-dev

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+var instance = new vidbg('#video', {
+    webm: "video/forest.webm",
+    poster: "video/forest.jpg", 
+    overlay: true, 
+  });
---
+++ js/scrollfull.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+    navigation: true,
+    responsiveWidth: 1280,
+    anchors: ['home', 'value-forest-1', 'value-forest-2'],
+    parallax: false,
+});
+ 
