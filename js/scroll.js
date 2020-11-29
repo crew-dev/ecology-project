9da97d688f81e8403c1d016974135b2bbe27a75c
Vlad Dedikov <69351396+crew-dev@users.noreply.github.com>
2020-11-29 16:09:28 +0300

    Various fixes

--- js/scroll.js
+++ js/scroll.js
@@ -2,6 +2,6 @@ $('a[href^="#"').on('click', function() {
   let href = $(this).attr('href');
   $('html, body').animate({
       scrollTop: $(href).offset().top
-  });
+  },650);
 	return false; 
 });
